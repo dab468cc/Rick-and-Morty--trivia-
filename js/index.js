07b519bcf7c309aa31c9babd5c438fffd592aeea
Daniel BEltran <danielbeltran468cc@gmail.com>
2023-02-15 22:57:39 -0700

    entrega final

--- js/index.js
+++ js/index.js
@@ -8,3 +8,57 @@ let swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+
+// let nombre=document.getElementById('usuario').value
+// document.getElementById("buttonName").addEventListener("click",mostrar)
+// function mostrar(){
+//   console.log(nombre)
+// } 
+// let buttonName=document.getElementById('buttonName')
+// buttonName.addEventListener('click',mostrar)
+// function mostrar(){
+//   console.log('hola desde el button')
+//   stopDefAct(e n)
+// }
+// function stopDefAct(e){
+//   e.preventDefault()
+
+// }
+
+let nombre='juan'
+
+localStorage.setItem('usuario',nombre)
+
+let usuarios=JSON.parse(localStorage.getItem('usuarios'))
+console.log(usuarios)
+
+// function crearTabla(array){
+//   const contenedorUsuarios=document.querySelector('#contUsuarios')
+//   array.forEach(array => {
+//     contenedorUsuarios.innerHTML=`
+//     <tr>
+//      <td>${array.nombre}</td>
+//      <td>${array.puntos}</td>
+//     </tr>
+// `
+//   });  
+  
+// }
+function crearTabla(array){
+  const contenedor = document.querySelector('#contUsuarios')
+  array.forEach(array =>{
+  const linea = document.createElement("tr")
+  linea.className="lineaUsuario"
+  // console.log(card)
+  linea.innerHTML=`
+  <td>${array.nombre}</td>  
+  <td>${array.puntos}</td>
+  `
+  contenedor.appendChild(linea)
+  
+  
+  });
+  }
+crearTabla(usuarios)
--- js/organizador.js
+++ js/organizador.js
@@ -1,7 +1,33 @@
-// console.log('hola mundo')
+// USUSARIO
+let jugadores=[]
+console.log(jugadores)
+jugadores=JSON.parse(localStorage.getItem('usuarios'))
+console.log(jugadores)
 
+class jugadorManager {
+    constructor(nombre,puntos){
+        this.nombre=nombre,
+        this.puntos=puntos
+        
+    }
+}
 
+function crearJugador(nombre,puntos){
+    let jugador= new jugadorManager(nombre,puntos)
+    jugadores.push(jugador)
+    console.log(jugadores)
+    localStorage.setItem('usuarios',JSON.stringify(jugadores))
+}
+let nombre=localStorage.getItem('usuario')
 
+console.log(nombre)
+//-------------- randomizador ------------
+let nRandom=0
+function random(){
+    nRandom = Math.random()*(10-1)+1
+    nRandom=parseInt(nRandom)
+    return nRandom
+}
 // VALIDACIONES
 let accP=1
 
@@ -12,64 +38,80 @@ function validarRespuestaSi(){
     if(bottonSi===true){
         alert('el bicho esta vivo respuesta correcta')
         puntos=accP++
+        accI++
         mostarPuntos()
     }else if(bottonSi===false){
-        alert('el bicho lamentablemente esta muerto,respuesta equivocada')    
+        alert('el bicho lamentablemente esta muerto,respuesta equivocada')  
+        accI++
+        mostarPuntos()  
     }
 }
 function validarRespuestaNo(){
     if(bottonNo===true){
         alert('el bicho lamentablemente esta muerto,respuesta correcta')
         puntos=accP++
+        accI++
         mostarPuntos()
         return accP
     }else if(bottonNo===false){
-        alert('el bicho esta vivo,respuesta equivocada')    
+        alert('el bicho esta vivo,respuesta equivocada') 
+        accI++
+        mostarPuntos()  
     }
 }
 function validarPregunta(array){
     const estado =array.status
     if(estado==="Alive"){
         bottonSi=true
-    }else{
+        bottonNo=false
+    }else if(estado==="Dead"||estado==="unknown"){
         bottonNo=true
+        bottonSi=false
     }
     console.log(bottonSi)
     console.log(bottonNo)
     console.log(estado)
     document.getElementById("opcionSi").addEventListener("click",validarRespuestaSi)
-        
-    
-    document.getElementById("opcionNo").addEventListener("click",validarRespuestaNo)
-        
-    
+    document.getElementById("opcionNo").addEventListener("click",validarRespuestaNo)  
 }
-
-// PUNTUACION
+// PUNTOS
 let puntos=accP
 function mostarPuntos(){
     console.log(puntos)
-
-    const contenedor=document.querySelector('#contPuntos')
-    contenedor.innerHTML=`
+    const contenedorPuntos=document.querySelector('#contPuntos')
+    contenedorPuntos.innerHTML=`
     Puntos:${puntos}
     `
     cp()
     finalizar()
 }
+function mostrarNombre(){
+    const contenedorNombre=document.querySelector('#contNombre')
+    contenedorNombre.innerHTML=`
+    ${nombre}
+`
+}
+mostrarNombre()
+// INTENTOS
+let accI=1
 
-
-//-------------- randomizador ------------
-let nRandom=0
-function random(){
-    nRandom = Math.random()*(10-1)+1
-    nRandom=parseInt(nRandom)
-    return nRandom
+// FINALIZAR
+function finalizar(){
+    if(puntos===10||accI===10){
+        alert(`test terminado,tu puntiacion es de ${puntos}`)
+        alert('volveremos al inicio')
+        crearJugador(nombre,puntos)
+        volver('../index.html')
+    }
+    
 }
 
-
-
-
+function volver(url){
+    window.open(url, '_blank')
+}
+   
+ 
+// CREAR PREGUNTAS
 function crearPregunta(array){
     const contenedor=document.querySelector('#contPreguntas')
     
@@ -87,10 +129,6 @@ function crearPregunta(array){
     </ul>
     `
 }
-
-    
-    
-   
 function cp(){
     random()
     const url="https://rickandmortyapi.com/api/character"
@@ -110,27 +148,3 @@ fetch(url)
 }
 cp()
 
-// USUSARIO
-let jugadores=[]
-class jugadorManager {
-    constructor(nombre,puntos){
-        this.nombre=nombre,
-        this.puntos=puntos
-        
-    }
-}
-
-function crearJugador(nombre,puntos){
-    let jugador= new jugadorManager(nombre,puntos)
-    jugadores.push(jugador)
-    console.log(jugadores)
-}
-
-// FINALIZAR
-function finalizar(){
-    if(puntos===2){
-        alert('test acabado')
-        crearJugador('juan',puntos)
-    }
-    
-}
